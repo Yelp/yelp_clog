ebf587e58b94a542b57710039c7125c64cb16c8e
Joshua Tiras <jot@yelp.com>
2017-03-29 11:04:27 -0700

    Address comments

--- tests/test_uwsgi_plugin.py
+++ tests/test_uwsgi_plugin.py
@@ -43,29 +43,33 @@ def uwsgi_plugin():
 @pytest.mark.usefixtures('uwsgi_plugin')
 class TestUwsgiPlugin(object):
 
-    def test_uwsgi_handle_msg_header(self, uwsgi_plugin):
+    def test_uwsgi_handle_valid_msg(self, uwsgi_plugin):
+        message = ('this is a fake stream', 'this is a fake line!')
+        mule_msg_data = uwsgi_plugin._encode_mule_msg(*message)
+        with mock.patch.object(uwsgi_plugin, '_orig_log_line') as orig_line:
+            with mock.patch.object(uwsgi_plugin, 'mule_msg_dispatcher') as dispatcher:
+                uwsgi_plugin._plugin_mule_msg_shim(mule_msg_data)
+                orig_line.assert_called_with(*map(six.b, message))
+                assert not dispatcher.called
+
+    def test_uwsgi_handle_invalid_msg_pass_thru(self, uwsgi_plugin):
         message = ('this is a fake stream', 'this is a fake line!')
         pickle_data = pickle.dumps(message)
-        mule_msg_data = uwsgi_plugin._encode_mule_msg(*message)
         with mock.patch.object(uwsgi_plugin, '_orig_log_line') as orig_line:
             with mock.patch.object(uwsgi_plugin, 'mule_msg_dispatcher') as dispatcher:
                 uwsgi_plugin._plugin_mule_msg_shim(pickle_data)
                 dispatcher.assert_called_with(pickle_data)
-                uwsgi_plugin._plugin_mule_msg_shim(mule_msg_data)
-                orig_line.assert_called_with(*map(six.b, message))
-
+                assert not orig_line.called
 
     def test_uwsgi_default_over_max_size(self, uwsgi_plugin):
         big_string = 's' * 65536
         assert uwsgi_plugin._mule_msg('blah', big_string) == False
-
 
     def test_uwsgi_default_on_failed_mule_msg(self, uwsgi_plugin):
         with mock.patch.object(uwsgi_plugin, '_orig_log_line') as orig_line:
             with mock.patch.object(uwsgi_plugin, '_mule_msg', return_value=False):
                 uwsgi_plugin.uwsgi_log_line('blah', 'test_message')
                 orig_line.assert_called_with('blah', 'test_message')
-
 
     def test_uwsgi_mule_msg_header_apply(self, uwsgi_plugin):
         args = ('test_stream', 'test_line')
@@ -74,7 +78,6 @@ class TestUwsgiPlugin(object):
         with mock.patch('uwsgi.mule_msg') as mm:
             uwsgi_plugin._mule_msg(*args, **kwargs)
             mm.assert_called_with(expected_serialized)
-
 
     def test_uwsgi_mule_msg_header_apply_with_mule(self, uwsgi_plugin):
         args = ('test_stream', 'test_line')
